a37da8731e2f3ce7cd436ac079958583ef87dc43
Pham Hoang Khoi <khoiphamkevin2004@gmail.com>
2022-11-22 15:41:36 -0800

    if/else change the content of the text everytime we clicked it

--- Week12/scripts/script.js
+++ Week12/scripts/script.js
@@ -10,10 +10,21 @@ function changeBGPink(){
     document.body.classList.add("pinkBG")
 }
 
+function clickme1(){
+    if(clickButton.textContent === 'Click me !'){
+        clickButton.textContent = 'Clicked!'
+    }
+    else(clickButton.textContent = 'Click me !');
+}
+
+function updateImage(){
+    
+}
 
 
 // clickButton.addEventListener('click', clickme, {once:true});
 // clickButton.addEventListener('click', changeBGPink);
+clickButton.addEventListener('click', clickme1 )
 
 
 
