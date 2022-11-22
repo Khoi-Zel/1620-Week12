e195970b4ec62dcb67d3f3a7d2c9f0d9859d0b45
Pham Hoang Khoi <khoiphamkevin2004@gmail.com>
2022-11-22 15:23:31 -0800

    background color changed when clicked a button

--- Week12/scripts/script.js
+++ Week12/scripts/script.js
@@ -1,8 +1,19 @@
 const clickButton = document.querySelector('.click-me')
 
 function clickme(){
-    console.log('You clicked me.');
+    alert('You clicked me.');
+    // clickButton.removeEventListener('click', clickme);
 }
 
-clickButton.addEventListener('click', clickme);
+function changeBGPink(){
+    // document.body.style.backgroundColor = "pink";   # Method 1
+    document.body.classList.add("pinkBG")
+}
+
+
+
+// clickButton.addEventListener('click', clickme, {once:true});
+// clickButton.addEventListener('click', changeBGPink);
+
+
 
