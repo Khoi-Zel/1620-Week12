7322c32766d35fdc9b34378f4affe319ea4878ba
Pham Hoang Khoi <khoiphamkevin2004@gmail.com>
2022-11-22 15:58:24 -0800

    add image and insert src, alt, width, height to the raw HTML by Javascripts

--- Week12/scripts/script.js
+++ Week12/scripts/script.js
@@ -17,14 +17,19 @@ function clickme1(){
     else(clickButton.textContent = 'Click me !');
 }
 
+const images = document.querySelector('#shoppingCart')
 function updateImage(){
-    
+    images.src = 'Photo/Cart.png'
+    images.alt = 'Shopping Cart'
+    images.width = '50'
+    images.height = '50'
 }
 
 
 // clickButton.addEventListener('click', clickme, {once:true});
 // clickButton.addEventListener('click', changeBGPink);
-clickButton.addEventListener('click', clickme1 )
+// clickButton.addEventListener('click', clickme1 );
+clickButton.addEventListener('click', updateImage)
 
 
 
